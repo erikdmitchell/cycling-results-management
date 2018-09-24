406a898657492dfce6e6b6c7d4dbfd6a71bddb9c
Erik <erikdmitchell@gmail.com>
2018-09-24 15:11:06 -0400

    added changed class name

--- class-cycling-results-management.php
+++ class-cycling-results-management.php
@@ -109,7 +109,7 @@ final class Cycling_Results_Management {
         include_once( CRM_PATH . 'class-crm-riders.php' ); // our riders functions
         include_once( CRM_PATH . 'classes/rider-rankings-query.php' ); // rider rankings query class
         include_once( CRM_PATH . 'classes/seasons.php' );
-        include_once( CRM_PATH . 'class-uci-rankings.php' );
+        include_once( CRM_PATH . 'admin/class-uci-rankings-admin.php' );
 
         include_once( CRM_PATH . 'functions/ajax.php' ); // ajax functions
         include_once( CRM_PATH . 'functions/races.php' ); // races functions
