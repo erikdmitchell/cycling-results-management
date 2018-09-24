73dba356f58774ae5a6225e57e7cf7c385523e75
Erik <erikdmitchell@gmail.com>
2018-09-24 15:51:53 -0400

    working on expanding riders query

--- functions/wp-query.php
+++ functions/wp-query.php
@@ -1,6 +1,14 @@
 <?php
 
-function riders_the_posts_details( $posts, $query ) {
+/**
+ * Append riders details to riders post type.
+ * 
+ * @access public
+ * @param mixed $posts
+ * @param mixed $query
+ * @return void
+ */
+function crm_riders_posts_details( $posts, $query ) {
     global $uci_riders;
 
     if ( $query->query_vars['post_type'] != 'riders' ) {
@@ -8,20 +16,28 @@ function riders_the_posts_details( $posts, $query ) {
     }
 
     foreach ( $posts as $post ) :
-        $post->nat = uci_get_first_term( $post->ID, 'country' );
-        $post->tiwtter = get_post_meta( $post->ID, '_rider_twitter', true );
-
-        if ( $query->get( 'ranking' ) ) :
-            $post->rank = $uci_riders->get_rider_rank( $post->ID );
-        endif;
-    endforeach;
+        $post->nat = uci_get_first_term( $post->ID, 'crm_country' );
+        $post->rank = $uci_riders->get_rider_rank( $post->ID );
+/*
+        $post->results = uci_results_get_rider_results(
+            array(
+                'rider_id' => $rider_id,
+                'race_ids' => $race_ids,
+                'season' => $results_season,
+            )
+        );
+        $post->last_result = $uci_riders->rider_last_race_result( $rider_id );
+        $post->rank = $uci_riders->get_rider_rank( $rider_id );
+        $post->stats = uci_results_get_rider_stats( $rider_id );
+*/
+    endforeach;   
 
     return $posts;
 }
-add_action( 'the_posts', 'riders_the_posts_details', 10, 2 );
+add_action( 'the_posts', 'crm_riders_posts_details', 10, 2 );
 
 /**
- * Append race details to races post
+ * Append race details to races post type.
  *
  * @access public
  * @param mixed $posts
