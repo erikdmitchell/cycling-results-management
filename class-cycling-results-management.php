ccea225bb4a54fad0bcfed9004cbca0dff23725e
Erik <erikdmitchell@gmail.com>
2018-09-25 11:27:24 -0400

    removed seasons class

--- class-cycling-results-management.php
+++ class-cycling-results-management.php
@@ -122,7 +122,6 @@ final class Cycling_Results_Management {
         include_once( CRM_PATH . 'class-crm-riders.php' ); // our riders functions
 
         include_once( CRM_PATH . 'classes/rider-rankings-query.php' ); // rider rankings query class
-        include_once( CRM_PATH . 'classes/seasons.php' );
 
         include_once( CRM_PATH . 'admin/class-uci-rankings-admin.php' );
         include_once( CRM_PATH . 'class-uci-rankings.php' );
