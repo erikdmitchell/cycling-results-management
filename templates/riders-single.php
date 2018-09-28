761a62c3553a028186ee6773d1a1ee34afc3d471
Erik <erikdmitchell@gmail.com>
2018-09-28 09:54:32 -0400

    updated rider results output

--- templates/riders-single.php
+++ templates/riders-single.php
@@ -13,36 +13,20 @@ get_header(); ?>
     <?php print_r( $post ); ?>
 </pre>
 
-<?php
-    /*
-global $uci_riders;
+<div class="container uci-results uci-results-rider">
 
-$rider = $uci_riders->get_rider(
-    array(
-        'rider_id' => get_query_var( 'rider_slug' ),
-        'results' => true,
-        'results_season' => '',
-        'ranking' => true,
-        'stats' => true,
-    )
-);
-*/
-?>
+    <?php if ( $post ) : ?>
+        <div id="rider-<?php echo $post->ID; ?>" class="row rider-stats">
+            <div class="col-md-4 general">
+                <?php the_title('<h1 class="page-title">', '</h1>'); ?>
 
-<div class="em-container uci-results uci-results-rider">
-
-    <?php if ( $rider ) : ?>
-        <div id="rider-<?php echo $rider->ID; ?>" class="em-row rider-stats">
-            <div class="em-col-md-4 general">
-                <h1 class="page-title"><?php echo $rider->post_title; ?></h1>
-
-                <div class="country"><span class="">Nationality:</span> <a href="<?php echo crm_country_url( $rider->nat ); ?>"><?php echo uci_results_get_country_flag( $rider->nat ); ?></a></div>
+                <div class="country"><span class="">Nationality:</span><?php echo uci_results_get_country_flag( $post->nat ); ?></div>
             </div>
             
             <?php foreach ( $rider->stats as $slug => $stats ) : ?>
                 <?php echo uci_results_stats_info( $slug )->name; ?>
                 <div class="rank"><span class="">Ranking:</span> <?php echo $rider->rank->rank; ?></div>
-                <div class="em-col-md-4 championships">
+                <div class="col-md-4 championships">
                     <h4>Championships</h4>
     
                     <div class="world-titles"><span class="">World Titles:</span> <?php crm_display_total( $stats->world_champs ); ?></div>
@@ -50,7 +34,7 @@ $rider = $uci_riders->get_rider(
                     <div class="superprestige-titles"><span class="">Superprestige Titles:</span> <?php crm_display_total( $stats->superprestige_titles ); ?></div>
                     <div class="bpost-bank-titles"><span class="">Gva/BPost Bank Titles:</span> <?php crm_display_total( $stats->gva_bpost_bank_titles ); ?></div>
                 </div>
-                <div class="em-col-md-4 top-results">
+                <div class="col-md-4 top-results">
                     <h4>Top Results</h4>
     
                     <div class="wins"><span class="">Wins:</span> <?php crm_display_total( $stats->wins ); ?></div>
@@ -62,27 +46,27 @@ $rider = $uci_riders->get_rider(
             <?php endforeach; ?>
         </div>
 
-        <?php if ( isset( $rider->results ) && count( $rider->results ) ) : ?>
+        <?php if ( isset( $post->results ) && count( $post->results ) ) : ?>
             <div class="single-rider-results">
                 <h3>Results</h3>
 
-                <div class="em-row header">
-                    <div class="em-col-md-2 race-date">Date</div>
-                    <div class="em-col-md-5 race-name">Event</div>
-                    <div class="em-col-md-1 rider-place">Place</div>
-                    <div class="em-col-md-1 rider-points">Points</div>
-                    <div class="em-col-md-1 race-class">Class</div>
-                    <div class="em-col-md-2 race-season">Season</div>
+                <div class="row header">
+                    <div class="col-md-2 race-date">Date</div>
+                    <div class="col-md-5 race-name">Event</div>
+                    <div class="col-md-1 rider-place">Place</div>
+                    <div class="col-md-1 rider-points">Points</div>
+                    <div class="col-md-1 race-class">Class</div>
+                    <div class="col-md-2 race-season">Season</div>
                 </div>
 
-                <?php foreach ( $rider->results as $result ) : ?>
-                    <div class="em-row">
-                        <div class="em-col-md-2 race-date"><?php echo date( get_option( 'date_format' ), strtotime( $result['race_date'] ) ); ?></div>
-                        <div class="em-col-md-5 race-name"><a href="<?php uci_results_race_url( $result['race_id'] ); ?>"><?php echo $result['race_name']; ?></a></div>
-                        <div class="em-col-md-1 rider-place"><?php echo $result['place']; ?></div>
-                        <div class="em-col-md-1 rider-points"><?php echo $result['par']; ?></div>
-                        <div class="em-col-md-1 race-class"><?php echo $result['race_class']; ?></div>
-                        <div class="em-col-md-2 race-season"><?php echo $result['race_season']; ?></div>
+                <?php foreach ( $post->results as $result ) : ?>
+                    <div class="row">
+                        <div class="col-md-2 race-date"><?php echo date( get_option( 'date_format' ), strtotime( $result['race_date'] ) ); ?></div>
+                        <div class="col-md-5 race-name"><a href="<?php uci_results_race_url( $result['race_id'] ); ?>"><?php echo $result['race_name']; ?></a></div>
+                        <div class="col-md-1 rider-place"><?php echo $result['place']; ?></div>
+                        <div class="col-md-1 rider-points"><?php echo $result['uci_points']; ?></div>
+                        <div class="col-md-1 race-class"><?php echo $result['race_class']; ?></div>
+                        <div class="col-md-2 race-season"><?php echo $result['race_season']; ?></div>
                     </div>
                 <?php endforeach; ?>
 
